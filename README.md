4aef252449456f74c185f598d95f64e30011e6ec
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2020-01-17 16:21:40 -0800

    updated webpack-template for more sensible usage. separate node build scripts instead of separate config files.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webpack-template
-My template for using webpack with separate dev and config files.
+My template for using webpack with separate dist/ and dev/ build scripts.
+
+Comments in webpack give options for versioning, and Vue-loader plugins.
---
+++ webpack.config.mjs
@@ -0,0 +1,85 @@
+const path = require( 'path' );
+const CopyPlugin = require( 'copy-webpack-plugin');
+
+const webpack = require('webpack');
+
+//const VueLoader = require('vue-loader/lib/plugin');
+//var VERS_STR = execSync('git rev-list HEAD --count').toString()
+
+module.exports = (enc, argv)=> {
+
+	// intended buildpath from script.
+	const buildpath = argv['buildpath'];
+	// absolute buildpath in system.
+	const absPath = path.resolve( __dirname, buildpath );
+
+	return {
+
+	mode: env.production ? "production" : 'development',
+	entry:{
+		name:"./src/index.js"
+	},
+	module: {
+
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			}
+		],
+	},
+	plugins: [
+		/*new VueLoader({
+			compilerOptions:{
+				whitespace:'condense'
+			}
+		}),*/
+		new webpack.DefinePlugin({
+		__DEBUG:true,
+		__CHEATS:true,
+		__DIST:env.production ? true : false,
+		__SAVE:null
+	}),
+	new CopyPlugin([
+
+		{
+			from:'index.html',
+			to:absPath
+		},
+		{
+			from:'data',
+			to:path.resolve( absPath, 'data')
+		},
+		{
+			from:'css',
+			to:path.resolve( absPath, 'css' )
+		}
+	])
+	],
+	output:{
+
+		filename: "[name].js",
+		chunkFilename: "[name].bundle.js",
+		path:path.resolve(__dirname, buildpath, 'js/' ),
+		publicPath:'js/',
+		library: "[name]"
+	},
+	resolve:{
+		modules:[
+			path.resolve( __dirname, "src"),
+			"node_modules"
+		],
+
+		alias:{
+			'config':'config',
+			"data":"../data",
+			'ui': 'ui'
+		}
+	}
+};
+
+};
